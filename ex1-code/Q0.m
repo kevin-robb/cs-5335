9b837685ad0f1d03f892624e4f93f4ef34e11964
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-04 14:37:55 -0500

    Finish Q1.

--- ex1-code/Q0.m
+++ ex1-code/Q0.m
@@ -2,5 +2,6 @@
 
 function q = Q0()
     % Change this configuration to visualize the arm's structure
-    q = [0 -pi/4 0 pi/2 0 pi/2 0];
+    %q = [0 -pi/4 0 pi/2 0 pi/2 0];
+    q = [-pi/4 pi/4 pi/4 pi/2 0 pi/2 0];
 end
--- ex1-code/Q1.m
+++ ex1-code/Q1.m
@@ -5,5 +5,9 @@
 %              (orientation is to be ignored)
 
 function q = Q1(f, position)
-
+    % the position must be converted to a homogenous matrix, T.
+    %T = [1,0,0,position(1);0,1,0,position(2);0,0,1,position(3);0,0,0,1];
+    T = transl(position(1),position(2),position(3));
+    % since we care only about pos and not orientation, we will use a mask.
+    q = f.ikine(T,'m',[1 1 1 0 0 0]);
 end
