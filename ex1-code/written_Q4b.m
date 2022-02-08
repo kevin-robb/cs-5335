be78ca36e7e30f75ce02f62478bdfc96fa2fd9bd
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-07 21:01:09 -0500

    Add figure of arm for Q4b at [0,0,1] pose.

--- ex1-code/written_Q4b.m
+++ ex1-code/written_Q4b.m
@@ -2,12 +2,12 @@
 syms th1 th2 th3 L1 L2
 % create the arm in 4-b using ETS3.
 import ETS3.*
-%L1 = 1; L2 = 1; th3 = 1;
+L1 = 1; L2 = 1; th3 = 1;
 % first two joints
 E3 = Tz(L1) * Rz('q1') * Ry(pi/2) * Rx(pi) * Rz(pi/2) * Rz('q2') * Tx(L2);
 % prismatic joint
 E3 = E3 * Ry(pi/2) * Rz(pi/2) * Tz(th3);
-%E3.teach()
+E3.teach()
 simplify(E3.fkine([th1 th2]))
 
 % my result from doing it manually:
