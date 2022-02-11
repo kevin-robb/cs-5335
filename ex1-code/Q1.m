64dd6a62a3f11dc941750f1c332108f1b7612d47
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-11 14:03:21 -0500

    Fix Q1 weirdness by changing mask param from 'm' to 'mask'.

--- ex1-code/Q1.m
+++ ex1-code/Q1.m
@@ -6,13 +6,7 @@
 
 function q = Q1(f, position)
     % the position must be converted to a homogenous matrix, T.
-    %T = transl(position(1),position(2),position(3));
+    T = transl(position(1),position(2),position(3));
     % since we care only about pos and not orientation, we will use a mask.
-    %q = f.ikine(T,'m',[1 1 1 0 0 0]);
-    
-    % the above code works fine on its own, but gives errors when
-    % the setup for Q4 runs in ex1. since I can't change that code, i'll
-    % just use Q2 to solve this problem and avoid ikine errors later.
-    qInit = [0 -pi/4 0 pi/2 0 pi/2 0 0 0]; %arbitrary initial pose
-    q = Q2(f, qInit, position);
+    q = f.ikine(T,'mask',[1 1 1 0 0 0]);
 end
