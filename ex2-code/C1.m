07eebd1bdb8e2ebea1551ba1df1aea9799430886
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-14 14:57:10 -0500

    Working C1 and q2poly (i think).

--- ex2-code/C1.m
+++ ex2-code/C1.m
@@ -6,29 +6,10 @@
 function C1(robot, q)
     % use the q2poly function to get plotting parameters.
     [poly1, poly2, pivot1, pivot2] = q2poly(robot, q);
-    %poly1 = params(1); poly2 = params(2); pivot1 = params(3); pivot2 = params(4);
-    % Plot the links
+    % Plot the links.
     plot(poly1, 'FaceColor', 'r');
     plot(poly2, 'FaceColor', 'b');
-    % Plot the pivot points
+    % Plot the pivot points.
     plot(pivot1(1), pivot1(2), 'k.', 'MarkerSize', 10);
     plot(pivot2(1), pivot2(2), 'k.', 'MarkerSize', 10);
-
-
-    % The following code plots the robot in configuration q = [0; 0].
-    % You should remove the following code and replace it with code that
-    % plots the robot links and pivots at the provided input configuration.
-%     % Translate frame origins
-%     origin1_at0 = robot.pivot1;
-%     origin2_at0 = origin1_at0 + robot.pivot2;
-%     % Compute link polygon corners
-%     link1_at0 = robot.link1 + origin1_at0;
-%     link2_at0 = robot.link2 + origin2_at0;
-%     % Plot the links
-%     plot(polyshape(link1_at0(1,:), link1_at0(2,:)), 'FaceColor', 'r');
-%     plot(polyshape(link2_at0(1,:), link2_at0(2,:)), 'FaceColor', 'b');
-%     % Plot the pivot points
-%     plot(origin1_at0(1), origin1_at0(2), 'k.', 'MarkerSize', 10);
-%     plot(origin2_at0(1), origin2_at0(2), 'k.', 'MarkerSize', 10);
-
 end
--- ex2-code/q2poly.m
+++ ex2-code/q2poly.m
@@ -15,9 +15,16 @@ function [poly1, poly2, pivot1, pivot2] = q2poly(robot, q)
     % calculate positions of pivot points.
     pivot1 = robot.pivot1; % always the same.
     pivot2 = pivot1 + [robot.pivot2(1)*cos(q(1)); robot.pivot2(1)*sin(q(1))];
+    % create transformation matrices for link params.
+    link1 = robot.link1; link2 = robot.link2;
+    R1 = rot2(q(1)); R2 = rot2(q(2));
+    for p = 1:4
+        link1(:,p) = R1 * link1(:,p);
+        link2(:,p) = R2 * R1 * link2(:,p);
+    end
     % shift link params by the pivot position.
-    link1 = robot.link1 + pivot1;
-    link2 = robot.link2 + pivot2;
+    link1 = link1 + pivot1;
+    link2 = link2 + pivot2;
     % create polyshapes based on link coords.
     poly1 = polyshape(link1(1,:),link1(2,:));
     poly2 = polyshape(link2(1,:),link2(2,:));
