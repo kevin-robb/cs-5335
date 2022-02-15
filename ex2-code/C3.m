c1e33de43f6b8486ef542249f227601fde16626e
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-14 19:59:20 -0500

    Got a start on C3, with all logic in place.

--- ex2-code/C3.m
+++ ex2-code/C3.m
@@ -12,5 +12,38 @@
 %                      >  2 otherwise
 
 function distances = C3(cspace, q_grid, q_goal)
-
+    % instantiate with all obstacle cells = 1, and all other cells = 0.
+    % i.e., start with cspace.
+    distances = cspace;
+    % find coords of the nearest configuration in grid to goal.
+    i_goal = round(q_goal / (2*pi) * size(q_grid,2));
+    % set value of goal cell.
+    distances(i_goal) = 2;
+    % expand out from the goal, computing distances.
+    cells_to_expand = [i_goal];
+    while ~isempty(cells_to_expand)
+        % add all adjacent unoccupied cells to the queue.
+        for r = -1:1
+        for c = -1:1
+            if distances(cells_to_expand(1) + [r;c]) == 0
+                % increment distance and assign to neighboring free cells.
+                distances(cells_to_expand(1) + [r;c]) = distances(cells_to_expand(1)) + 1;
+                % add these newly explored cells to the queue.
+                cells_to_expand = [cells_to_expand, cells_to_expand(1) + [r;c]];
+            end
+            % remove expanded cell from queue.
+            cells_to_expand(:,1) = [];
+        end
+        end
+    end
 end
+
+
+% % % testing
+% poly1 = polyshape([0 0 0.4 0.4],[1 0 0 1]);
+% poly2 = polyshape([0.75 1.25 1.25 0.75],[0.25 0.25 0.75 0.75]);
+% plot(poly1)
+% hold on
+% plot(poly2)
+% polyout = intersect(poly1,poly2)
+% polyout.Vertices
