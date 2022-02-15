5fd7f7bb8e80baae04f508e054ecdca805b3feca
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-14 19:13:14 -0500

    Q2 working properly now.

--- ex2-code/C2.m
+++ ex2-code/C2.m
@@ -11,7 +11,7 @@
 
 function cspace = C2(robot, obstacles, q_grid)
     % instantiate the config space with the desired dimensions.
-    cspace = q_grid + q_grid';
+    cspace = q_grid + q_grid'; %zeros(size(q_grid,2));
     % check each cell for collision and assign it 0 or 1.
     for i1 = 1:size(q_grid,2)
         for i2 = 1:size(q_grid,2)
@@ -24,12 +24,12 @@ function cspace = C2(robot, obstacles, q_grid)
             % (poly1 and poly2 are allowed to collide with each other.)
             for i_ob = 1:size(obstacles,2)
                 if in_collision
+                    % stop the loop if a collision is found.
                     break
                 end
-                obs_poly = obstacles(i_ob);
-                col1 = isempty(intersect(poly1, obs_poly).Vertices);
-                col2 = isempty(intersect(poly2, obs_poly).Vertices);
-                in_collision = in_collision || col1 || col2;
+                col1 = ~isempty(intersect(poly1, obstacles(i_ob)).Vertices);
+                col2 = ~isempty(intersect(poly2, obstacles(i_ob)).Vertices);
+                in_collision = col1 || col2;
             end
             % update the grid based on the result.
             if in_collision
@@ -40,16 +40,4 @@ function cspace = C2(robot, obstacles, q_grid)
         end
     end
 end
-
-
-
-
-% % testing
-% poly1 = polyshape([0 0 0.4 0.4],[1 0 0 1]);
-% poly2 = polyshape([0.75 1.25 1.25 0.75],[0.25 0.25 0.75 0.75]);
-% plot(poly1)
-% hold on
-% plot(poly2)
-% polyout = intersect(poly1,poly2)
-% polyout.Vertices
 
