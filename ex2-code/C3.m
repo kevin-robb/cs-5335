3bbc466871fe061fa90dedde70bd36b0e3edfc2a
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-14 22:37:00 -0500

    Add cspace wrapping with modulo to Q3 and Q4. Q4 logic is there but is getting stuck in a loop early on.

--- ex2-code/C3.m
+++ ex2-code/C3.m
@@ -15,7 +15,7 @@ function distances = C3(cspace, q_grid, q_goal)
     % instantiate with all obstacle cells = 1, and all other cells = 0.
     % i.e., start with cspace.
     distances = cspace;
-    % find coords of the nearest configuration in grid to goal.
+    % find coords of the nearest configuration in grid to q_goal.
     i_goal = round(q_goal / (2*pi) * size(q_grid,2));
     % set value of goal cell.
     distances(i_goal) = 2;
@@ -26,16 +26,18 @@ function distances = C3(cspace, q_grid, q_goal)
         % add all adjacent unoccupied cells to the queue.
         for r = -1:1
         for c = -1:1
-            try % use a try-catch since the indices may go out of range.
-            i_new_cell = i_cur_cell + [r;c];
-%             if i_new_cell(1)
+            % use modulo to allow neighbors to wrap around the cspace.
+            % do a weird hacky thing to prevent zeros messing us up.
+            i_new_cell = mod(i_cur_cell + [r;c] - [1;1], size(q_grid,2)) + [1;1];
+            % NOTE this makes the plot look different from the figure in
+            % the assignment pdf, but wrapping should be possible so I'll
+            % keep it this way. The plots match if I use a try/catch
+            % instead to prevent wrapping.
             if distances(i_new_cell(1),i_new_cell(2)) == 0
                 % increment distance and assign to neighboring free cells.
                 distances(i_new_cell(1),i_new_cell(2)) = distances(i_cur_cell(1),i_cur_cell(2)) + 1;
                 % add these newly explored cells to the queue.
                 cells_to_expand = [cells_to_expand, i_new_cell];
-            end
-            catch % don't do anything, just pass.
             end
         end
         end
--- ex2-code/C4.m
+++ ex2-code/C4.m
@@ -15,5 +15,32 @@
 %                 the grid cell containing q_goal.
 
 function path = C4(distances, q_grid, q_start)
-
+    % find coords of the nearest configuration in grid to q_start.
+    i_cur = round(q_start / (2*pi) * size(q_grid,2));
+    path = [i_cur(1),i_cur(2)];
+    % loop through distances to generate greedy path to goal.
+    while ~(distances(i_cur(1),i_cur(2)) == 2)
+        % i.e., until our current cell is the goal.
+        % check distance values of all neighboring cells.
+        i_min_nbr_dist = i_cur;
+        for r = -1:1
+        for c = -1:1
+            % use modulo to allow path to wrap around the cspace.
+            % do a weird hacky thing to prevent zeros messing us up.
+%             i_nbr = i_cur + [r;c];
+            i_nbr = mod(i_cur + [r;c] - [1;1], size(q_grid,2)) + [1;1];
+            % don't choose occluded neighbors.
+            if distances(i_nbr(1),i_nbr(1)) == 1
+                break
+            end
+            if distances(i_nbr(1),i_nbr(2)) <= distances(i_min_nbr_dist(1),i_min_nbr_dist(2))
+                % found new best neighbor to go to.
+                i_min_nbr_dist = i_nbr;
+            end
+        end
+        end
+        % add the best neighbor to the path and change our current cell to it.
+        i_cur = i_min_nbr_dist;
+        path = [path; i_cur']
+    end
 end
