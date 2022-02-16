b918b800a34592d9ba877d778eae2dc62a7f4679
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-16 13:58:48 -0500

    PRM graph being generated and looks good?

--- ex2-code/M2.m
+++ ex2-code/M2.m
@@ -21,5 +21,60 @@
 %                      symmetric: adjacency(i,j) == adjacency(j,i)
 
 function [samples, adjacency] = M2(robot, q_min, q_max, num_samples, num_neighbors, link_radius, sphere_centers, sphere_radii)
-
+    % generate vertices via unif rand sampling.
+    % same as M1 but check for collisions.
+    range = q_max - q_min;
+    samples = [];
+    while size(samples,1) < num_samples
+        % generate a random sample in bounds, and add it if no collision.
+        new_vert = rand(1, 4) .* range + q_min;
+        if ~check_collision(robot, new_vert, link_radius, sphere_centers, sphere_radii)
+            samples = [samples; new_vert];
+        end
+    end
+    % create a matrix with the distances between all pairs.
+    distances = zeros(num_samples,num_samples);
+    % note: this could be more efficient by taking advantage of symmetry,
+    % but this is just easier.
+    for i = 1:num_samples
+        for j = 1:num_samples 
+            if i == j
+                distances(i,j) = NaN;
+            else
+                distances(i,j) = norm(samples(j,:)-samples(i,:));
+            end
+        end
+    end
+    distances
+    % now we need to connect vertices with edges. each point will be
+    % connected to its nearest `num_neighbors` neighbors if
+    % collision-free paths exist.
+    adjacency = zeros(num_samples,num_samples);
+    for i = 1:num_samples 
+        nbrs = distances(i,:);
+        for n = 1:num_neighbors
+            % find nearest neighbor.
+            [dist,j] = min(nbrs,[],'omitnan');
+            % prevent this neighbor from being chosen again.
+            nbrs(j) = NaN;
+            % check this path for no collisions.
+            if ~check_edge(robot, samples(i,:), samples(j,:), link_radius, sphere_centers, sphere_radii)
+                % add this entry to the adjacency map.
+                adjacency(i,j) = dist;
+                adjacency(j,i) = dist;
+            end
+        end
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
