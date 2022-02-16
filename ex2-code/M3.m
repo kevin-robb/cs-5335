70cb7d840cc61688696088362b5257e169fde71f
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-16 14:30:00 -0500

    M3 sort of done, but is finding a path through obstacles, so that needs to be checked in either M3 or M2.

--- ex2-code/M3.m
+++ ex2-code/M3.m
@@ -16,5 +16,72 @@
 %         path_found -> Boolean denoting whether a path was found
 
 function [path, path_found] = M3(robot, samples, adjacency, q_start, q_goal, link_radius, sphere_centers, sphere_radii)
+    % we first need to create a graph from our samples and adjacency map.
+    s = []; t = []; weights = [];
+    for i = 1:(size(samples,1)-1)
+        for j = (i+1):size(samples,1)
+            % only make an edge if they have a distance saved.
+            if adjacency(i,j) ~= 0
+                s = [s, i];
+                t = [t, j];
+                weights = [weights, adjacency(i,j)];
+            end
+        end
+    end
+    G = graph(s,t,weights);
+%     figure;
+%     plot(G,'EdgeLabel',G.Edges.Weight)
 
+    % get the distance from each node to the start and goal.
+    dist_to_start = [];
+    dist_to_goal = [];
+    for i = 1:size(samples,1)
+        dist_to_start = [dist_to_start; norm(samples(i,:)-q_start)];
+        dist_to_goal = [dist_to_goal; norm(samples(i,:)-q_goal)];
+    end
+    % find the closest node w/o collision to each.
+    node_start = 0; node_goal = 0;
+    while node_start == 0
+        [~,j] = min(dist_to_start,[],'omitnan');
+        dist_to_start(j) = NaN;
+        if ~check_edge(robot, q_start, samples(j,:), link_radius, sphere_centers, sphere_radii)
+            node_start = j;
+        end
+    end
+    while node_goal == 0
+        [~,j] = min(dist_to_goal,[],'omitnan');
+        dist_to_goal(j) = NaN;
+        if ~check_edge(robot, samples(j,:), q_goal, link_radius, sphere_centers, sphere_radii)
+            node_goal = j;
+        end
+    end
+    if node_start == 0 || node_goal == 0
+        % no path exists to the PRM from either the start or goal config.
+        path_found = false;
+        path = [];
+        return
+    end
+
+    % find the shortest path on the graph between these nodes.
+    node_path = shortestpath(G,node_start,node_goal);
+    % if the path is empty, no path exists.
+    if isempty(node_path)
+        path_found = false;
+        path = [];
+        return
+    end
+
+    % getting here means a path does exist.
+    path_found = true;
+    % convert the path from nodes to configurations.
+    path = q_start;
+    for p = 1:size(node_path)
+        path = [path; samples(node_path(p),:)];
+    end
+    path = [path; q_goal];
 end
+
+
+
+
+
