4387c45bca4fb9e8e6199bc03449f26cad4429ec
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-16 13:02:43 -0500

    Creating unif random distr of configurations in bounds. Not checking for collision.

--- ex2-code/M1.m
+++ ex2-code/M1.m
@@ -5,5 +5,13 @@
 %               all within joint limits
 
 function qs = M1(q_min, q_max, num_samples)
-
+    % the rand(m,n) function generates an m x n matrix of uniformly
+    % distributed random values in the range (0,1). We can transform each
+    % column to align with its corresponding joint angle range.
+    range = q_max - q_min;
+    qs = rand(num_samples, 4) .* range + q_min;
 end
+
+
+%     % Check if configuration is within joint limits / in collision
+%     in_bounds = (all(q >= q_min) && all(q <= q_max));
