03dcc2c93a0bba42b5e9753511f43403c5b196e2
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-19 11:59:20 -0500

    Fix M4 RRT algorithm with hyperparameter tweaks. Add fail condition instead of infinite loop.

--- ex2-code/M4.m
+++ ex2-code/M4.m
@@ -15,31 +15,28 @@
 %         path_found -> Boolean denoting whether a path was found
 
 function [path, path_found] = M4(robot, q_min, q_max, q_start, q_goal, link_radius, sphere_centers, sphere_radii)
-    figure;
     % config parameters.
-    step_size = 0.01; freq_sample_goal = 0.1; tolerance = 0.05;
+    step_size = 0.05; freq_sample_goal = 0.5; tolerance = 0.1; max_nodes = 800;
     % we will refer to nodes with an index, which refers to an actual
     % configuration in the Nx4 `vertices` matrix.
     num_nodes = 1;
     vertices = q_start;
     % create the tree as a graph.
     tree = graph([1],[]);
-    plot(tree)
-    hold on
 
-    % init in case we fail.
+    % init in case we fail to find a path before hitting max_nodes.
     path = []; path_found = false;
 
-    % continue adding random nodes to the tree until we hit the goal.
-    while true
+    % continue adding random nodes to the tree until we hit the goal or the
+    % max allowable number of nodes.
+    while num_nodes < max_nodes
         if rand(1) <= freq_sample_goal
-            % sometimes sample the goal directly.
+            % sample the goal directly.
             q_target = q_goal;
         else
             % sample a random configuration in bounds.
             q_target = rand(1, 4) .* (q_max - q_min) + q_min;
         end
-        q_target
         % find nearest node to this point.
         q_near_i = 0; min_dist = 100;
         for i = 1:num_nodes
@@ -49,33 +46,35 @@ function [path, path_found] = M4(robot, q_min, q_max, q_start, q_goal, link_radi
                 q_near_i = i;
             end
         end
-        q_near = vertices(q_near_i,:)
+        q_near = vertices(q_near_i,:);
         % get the configuration `step_size` distance along the unit vector
         % from q_near to q_target.
         q_new = q_near + step_size * ((q_target - q_near)/min_dist);
         % check for collisions from q_near to q_new.
         if ~check_edge(robot, q_near, q_new, link_radius, sphere_centers, sphere_radii)
-            disp no_collision
+%             disp no_collision
             % add q_new to our tree.
             vertices = [vertices; q_new];
             num_nodes = num_nodes + 1;
             tree = addnode(tree,1);
             tree = addedge(tree,[q_near_i],[num_nodes]);
         end
-        plot(tree)
 
         % check if we have just added the goal to the tree, and finish if so.
         if norm(vertices(num_nodes,:) - q_goal) < tolerance
+            disp FOUND_GOAL
             % we know there is exactly 1 path because it's a tree.
             path_found = true;
             node_path = shortestpath(tree,1,num_nodes);
             for p = 1:size(node_path,2)
                 path = [path; vertices(node_path(1,p),:)];
             end
+            % explicitly ensure path ends at goal config.
+            path = [path; q_goal];
             return
         end
     end
-
+    disp FAILED_TO_FIND_PATH
 end
 
 %     figure;
