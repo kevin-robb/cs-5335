f6d4eb2ea4aa83b418e2b151bdff02f0de762e74
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-02-19 12:25:53 -0500

    M5 path smoothing is working, perhaps less efficiently than possible.

--- ex2-code/M5.m
+++ ex2-code/M5.m
@@ -12,5 +12,26 @@
 %                          waypoints may have been removed
 
 function smoothed_path = M5(robot, path, link_radius, sphere_centers, sphere_radii)
-
+    % go front to back and try to skip as many unnecessary waypoints as
+    % possible and build a more efficient path.
+    smoothed_path = [];
+    i = 1; % can't use for loop since I want to change the index sometimes.
+    while i < size(path,1)
+        i = i + 1;
+        % add link i to the path. it's necessary if we get here.
+        smoothed_path = [smoothed_path; path(i,:)];
+        % try to find a connection to a point as far away as possible.
+        for j = size(path,1):-1:(i+1)
+            if ~check_edge(robot, path(i,:), path(j,:), link_radius, sphere_centers, sphere_radii)
+                disp('found shortcut from i to j');
+                i
+                j
+                % a path exists, so add j directly to the list.
+                smoothed_path = [smoothed_path; path(j,:)];
+                % update i=j to skip all intermediary points
+                i = j;
+                break
+            end
+        end
+    end
 end
