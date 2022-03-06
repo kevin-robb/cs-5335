28e33f57c31b96b6a481748ec60d576478cd498b
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-03-06 16:59:55 -0500

    Fix errors with E1 EKF. Runs localization now (but just sucks).

--- ex3_code/E1.m
+++ ex3_code/E1.m
@@ -26,7 +26,7 @@ function [x_est, P_est] = E1(odo, zind, z, V, W, x0, P0, map)
     % do ekf propagation for all timesteps.
     % keep track of most recent state estimate.
     x_t = x0; P_t = P0;
-    for t = 1:T %(T-1)
+    for t = 1:(T-1)
         % get landmark index.
         lm_i = zind(1,t);
         % we treat odom as command (dist, heading)^T
@@ -47,36 +47,42 @@ function [x_est, P_est] = E1(odo, zind, z, V, W, x0, P0, map)
                        x_t(2) + (d_d + v_d)*sin(x_t(3));
                        x_t(3) + d_th + v_th];
         P_predicted = F_x * P_t * F_x' + F_v * V * F_v';
-        % if there's no landmark measurement, stop. equiv to K=0.
+        % check if there is a measurement on this timestep.
         if lm_i == 0
-            x_est{1,t} = x_predicted;
-            P_est{1,t} = P_predicted;
-            continue
+            % if no landmark meas, treat like K=0 and just use prediction.
+            x_t = x_predicted;
+            P_t = P_predicted;
+        else
+            % get true landmark position, and our rb meas of it.
+            p_i = map.landmark(lm_i);
+            rb = z{1,t}; %r = rb(1); b = rb(2);
+            % compute remaining jacobian matrices.
+            dist = sqrt((p_i(1)-x_t(1))^2 + (p_i(2)-x_t(2))^2);
+            H_x = [-(p_i(1)-x_t(1))/dist, -(p_i(2)-x_t(2))/dist, 0;
+                   (p_i(2)-x_t(2))/dist^2, -(p_i(1)-x_t(1))/dist^2, -1];
+            H_w = [1, 0; 0, 1];
+            % compute innovation.
+            z_est = [sqrt((p_i(1)-x_t(1))^2 + (p_i(2)-x_t(2))^2);
+                    atan2(p_i(2)-x_t(2), p_i(1)-x_t(1)) - x_t(3)];
+            nu = rb - z_est + [w_r; w_b];
+            % compute kalman gain.
+            S = H_x * P_predicted * H_x' + H_w * W * H_w';
+            K = P_predicted * H_x' / S; %inv(S)
+            % update step.
+            x_t = x_predicted + K * nu;
+            P_t = P_predicted - K * H_x * P_predicted;
         end
-        % get true landmark position, and our rb meas of it.
-        p_i = map.landmark(lm_i);
-        rb = z{1,t}; %r = rb(1); b = rb(2);
-        % compute remaining jacobian matrices.
-        dist = sqrt((p_i(1)-x_t(1))^2 + (p_i(2)-x_t(2))^2);
-        H_x = [-(p_i(1)-x_t(1))/dist, -(p_i(2)-x_t(2))/dist, 0;
-               (p_i(2)-x_t(2))/dist^2, -(p_i(1)-x_t(1))/dist^2, -1];
-        H_w = [1, 0; 0, 1];
-        % compute innovation.
-        z_est = [sqrt((p_i(1)-x_t(1))^2 + (p_i(2)-x_t(2))^2);
-                atan2(p_i(2)-x_t(2), p_i(1)-x_t(1)) - x_t(3)];
-        nu = rb - z_est + [w_r; w_b];
-        % compute kalman gain.
-        S = H_x * P_predicted * H_x' + H_w * W * H_w';
-        K = P_predicted * H_x' / S; %inv(S)
-        % update step.
-        x_t = x_predicted + K * nu;
-        P_t = P_predicted - K * H_x * P_predicted;
+        % set our estimate for time t+1.
         x_est{t+1} = x_t;
         P_est{t+1} = P_t;
     end
 %     disp(x_est)
-    celldisp(x_est)
+%     celldisp(x_est)
 %     disp(P_est)
+%     for t = 1:T
+%         t
+%         x_est{t}(1:3)
+%     end
 end
 % may be helpful to use functions atan2, angdiff.
 % I'm assuming from the data format that we can only observe one
