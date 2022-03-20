acb002ab1f7beaa391fa1e0d31ac4658189d67c2
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-03-20 11:19:05 -0400

    Implement manual trajectory for high-accuracy SLAM.

--- ex4_code/E6.m
+++ ex4_code/E6.m
@@ -7,47 +7,56 @@
 function [x_truth, odo_truth] = E6(x0, T)
     % odom constraints:
     odom_d_max = 0.1; odom_th_max = 0.0546;
-    % the map is not passed and we obviously need it,
-    % so assume the default map.
+    % the map is not passed and we obviously need it, so assume the default map.
     rng(0); map = LandmarkMap(20);
     % instantiate the trajectory.
     x_truth = zeros(3,T); odo_truth = zeros(2,T);
-    % first pass attempt: just design a trajectory to pass through all
-    % landmarks. treat as travelling salesman problem, and use NN heuristic.
-    % choose nearest landmark to initial pose as 1st node.
-    cur_goal = 1; cur_dist = norm(map.landmark(cur_goal) - x0(1:2));
-    for i_lm = 1:map.nlandmarks
-        if norm(map.landmark(i_lm) - x0(1:2)) < cur_dist
-            cur_goal = i_lm;
-            cur_dist = norm(map.landmark(i_lm) - x0(1:2));
-        end
-    end
 
-    % build directed graph using nearest neighbors approach.
-    cur_node = cur_goal; % start where we decided above.
-    % store path of landmark indexes to visit in order.
-    lm_path = [cur_node]; % track path as a 1xN vector.
-    unvisited = 1:map.nlandmarks; 
-    unvisited = unvisited(unvisited~=cur_node);
-    cur_goal = 0; cur_dist = -1;
-    while ~isempty(unvisited)
-        % find nearest neighbor.
-        for i_lm = unvisited
-            new_dist = norm(map.landmark(i_lm) - map.landmark(cur_node));
-            if cur_dist < 0 || new_dist < cur_dist
+    % set which trajectory will be used. 1=TSP, 2=manual.
+    TRAJ_CHOICE = 1;
+
+    lm_path = [];
+    if TRAJ_CHOICE == 1
+        % for any map, we can design a trajectory to pass through all landmarks,
+        % by treating it as travelling salesman problem, and use NN heuristic.
+        % choose nearest landmark to initial pose as 1st node.
+        cur_goal = 1; cur_dist = norm(map.landmark(cur_goal) - x0(1:2));
+        for i_lm = 1:map.nlandmarks
+            if norm(map.landmark(i_lm) - x0(1:2)) < cur_dist
                 cur_goal = i_lm;
-                cur_dist = new_dist;
+                cur_dist = norm(map.landmark(i_lm) - x0(1:2));
             end
         end
-        % add this edge.
-        lm_path = [lm_path, cur_goal];
-        % mark node as visited and update current.
-        cur_node = cur_goal;
+    
+        % build directed graph using nearest neighbors approach.
+        cur_node = cur_goal; % start where we decided above.
+        % store path of landmark indexes to visit in order.
+        lm_path = [cur_node]; % track path as a 1xN vector.
+        unvisited = 1:map.nlandmarks; 
         unvisited = unvisited(unvisited~=cur_node);
         cur_goal = 0; cur_dist = -1;
+        while ~isempty(unvisited)
+            % find nearest neighbor.
+            for i_lm = unvisited
+                new_dist = norm(map.landmark(i_lm) - map.landmark(cur_node));
+                if cur_dist < 0 || new_dist < cur_dist
+                    cur_goal = i_lm;
+                    cur_dist = new_dist;
+                end
+            end
+            % add this edge.
+            lm_path = [lm_path, cur_goal];
+            % mark node as visited and update current.
+            cur_node = cur_goal;
+            unvisited = unvisited(unvisited~=cur_node);
+            cur_goal = 0; cur_dist = -1;
+        end
+    elseif TRAJ_CHOICE == 2
+        % manually choose waypoints for this map such that the veh will
+        % pass through only high density areas.
+        lm_path = [14,19,16,8,7,12,1];
     end
-
-    % traverse our graph to generate actual trajectory.
+    % traverse our graph to generate an actual trajectory.
     t = 1; x_v = x0;
     while t <= T
         % first entry in lm_path is always the goal.
