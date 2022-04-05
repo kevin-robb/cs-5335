02aaf49a038adc581eb7c3c128eb504c6cac6cbd
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-04-05 11:26:11 -0400

    Reformat to compare each image to template, to get only feature matches corresponding to the book.

--- ex5_code/v1.m
+++ ex5_code/v1.m
@@ -10,23 +10,29 @@ num_images = numel(scene.Files);
 % read in template of book cover.
 I_template = iread('filename1.jpg','mono','double');
 %%%%%%%%%%%%%%%%%% DETECT FEATURES IN EACH IMAGE %%%%%%%%%%%%%%%%%%
-% sf_prev = null; I_prev = null;
+% first get features from template.
+sf_template = isurf(I_template, 'nfeat', 200);
+% get features in each image and compare to template.
 for n = 1:num_images
     I = readimage(scene, n);
     % detect SURF features.
     sf = isurf(I, 'nfeat', 200);
     % show features on image.
     idisp(I); sf.plot_scale('g');
+    % match features with template.
+    [m,correspondences] = sf_template.match(sf);
+    % TODO use RANSAC to compute homography between template and I.
+    % TODO decompose homography into rotation and translation.
     if n == 1
-        I_prev = I;
-        sf_prev = sf;
+        tf_prev = tf;
+        correspondences_prev = correspondences;
+%         I_prev = I;
+%         sf_prev = sf;
         continue
     end
-    % match pairs of images.
-    [m,correspondences] = sf_prev.match(sf);
-    % show matches.
-    idisp({I_prev, I}); %,'dark'
-    m.subset(100).plot('w');
-    % TODO use RANSAC to compute homography between I_prev and I.
-    % TODO decompose homography into rotation and translation.
+    % use tf_prev and tf to compute I_prev -> I.
+    % TODO if tf in SE(3),
+    tf_images = inv(tf_prev) * tf;
+    % TODO to display matches, maybe use correspondences for each to match
+    % features between the images, using template as intermediary.
 end
