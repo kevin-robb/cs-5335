1751a90208375f2b0678d726ebd37616a0ee8c5c
Kevin Robb <kevin.robb@alumni.ou.edu>
2022-04-04 20:06:13 -0400

    Change traj init to improve optimality.

--- ex5_code/opt_lightdark.m
+++ ex5_code/opt_lightdark.m
@@ -2,21 +2,25 @@
 %%%%%%%%%%%%%%%%%%%%%%%% CONSTRAINTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 x_start = [2; 2; 5]; x_goal = [4; 0; 0.01];
 T = 20; % # of timesteps.
-x = zeros(3*T,1); % 3Tx1. format: [x1,x2,..,xT,y1,y2,...,yT,v1,...,vT].
+% x = ones(3*T,1); % 3Tx1. format: [x1,x2,..,xT,y1,y2,...,yT,v1,...,vT].
+x = [zeros(T,1)+x_start(1);zeros(T,1)+x_start(2);zeros(T,1)+x_start(3)];
 % need to make matrices Aeq and Beq s.t. Aeq * u = Beq for valid trajectories.
-% i.e., start and end should match x_start, x_goal.
-Aeq = zeros(6,3*T); % select out the points we want to check.
+% i.e., start and end should match x_start, x_goal. initial variance too.
+Aeq = zeros(5,3*T); % select out the points we want to check.
 Aeq(1,1) = 1; Aeq(2,T+1) = 1; Aeq(3,2*T+1) = 1;
-Aeq(4,T) = 1; Aeq(5,2*T) = 1; Aeq(6,3*T) = 1;
-Beq = [x_start; x_goal]; % 6x1.
+Aeq(4,T) = 1; Aeq(5,2*T) = 1; %Aeq(6,3*T) = 1;
+Beq = [x_start; x_goal(1:2)]; % 5x1.
 % ensure variance is positive and greater than epsilon.
 variance_epsilon = 0.0001; % minimum allowable variance.
 A = -eye(3*T); A = A(2*T+1:3*T, 1:3*T); % Tx3T.
 B = zeros(T,1) - variance_epsilon; % Tx1.
+% add a row to ensure the final variance <= specified val.
+A = [A; zeros(1,3*T-1), 1];
+B = [B; x_goal(3)];
 % set nonlinear constraint function.
 max_control = 0.5; % constraint for max of a single control.
 process_variance = 0.01; % variance increase in prediction step.
-nl_con_fn = @(x) nl_con_c(x, max_control, process_variance);
+nl_con_fn = @(x) nl_con_c(x, max_control, process_variance, variance_epsilon);
 
 %%%%%%%%%%%%%%%%%%%% CHOOSE COST FUNCTION %%%%%%%%%%%%%%%%%%%%%%%%
 cost_fun = @(x) cost_fn_c(x);
@@ -52,12 +56,14 @@ function cost = cost_fn_c(x)
     % cost = square of each component summed.
     diffs = [x(2:T) - x(1:T-1); x(T+2:2*T) - x(T+1:2*T-1)];
     cost = sum(diffs .* diffs);
+    % add the variance to the cost.
+%     cost = cost + sum(x(2*T+1:3*T));
 end
 
 %%%%%%%%%%%%%%%%%% NONLINEAR CONSTRAINT FUNCTION %%%%%%%%%%%%%%%%%%%%%%%%%
 % OPT-c light-dark domain.
 % @param x = 3Tx1 matrix of positions + variances.
-function [c,ceq] = nl_con_c(x, max_control, process_variance)
+function [c,ceq] = nl_con_c(x, max_control, process_variance, epsilon)
     % create cons s.t. c <= 0, ceq = 0.
     T = round(size(x,1) / 3);
     % each position must be no more than max_control from the prev.
@@ -68,7 +74,12 @@ function [c,ceq] = nl_con_c(x, max_control, process_variance)
     ceq = zeros(T-1,1);
     for i = 1:T-1
         % ensure the variance at the next state matches the prediction.
-        ceq(i) = v(x(i), x(2*T+i)) - x(2*T+i+1);
+        v_next = v(x(i), x(2*T+i));
+        % however, do not allow to it shrink below epsilon.
+        if v_next < epsilon
+            v_next = epsilon;
+        end
+        ceq(i) = v_next - x(2*T+i+1);
     end
 end
 
